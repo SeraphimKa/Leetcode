29cbcf25633cb9137de0126647fb18e33a705044
SeraphimKa <fandomethor@gmail.com>
2023-04-16 20:42:16 +0300

    some changes

--- MaximumValueOfKCoinsFromPiles.js
+++ MaximumValueOfKCoinsFromPiles.js
@@ -6,24 +6,48 @@
 //What is the maximum value we can take given piles and k?
 
 var maxValueOfCoins = function (piles, k) {
-    let dp = new Array(piles.length + 1).fill(0).map(() => new Array(k).fill(0))
+    let dp = new Array(piles.length + 1).fill(-Infinity).map(() => new Array(k).fill(-Infinity)) //dp has 1 more row than piles, columns = max number of moves
+    let n = piles.length;
     let j;
-    for (let i = piles.length - 1; i >= 0; i--) {
+    let colMax; //Max coins, current row + 
+    let colBack = 0; //Counter of coins left, 1 column back
+    let colBackPoint; //Last cell, 1 column back
+    let colBackGrab; //Coins grabbed from 1 column back
+    for (let i = n - 1; i >= 0; i--) { //Starting from next to last so we can use the extra row to compare
+        colMax = 0;
+        colBackGrab = 0;
+        colBackPoint = 0;
         for (j = 0; j < k; j++) {
 
-            if (j == 0)
-                dp[i][j] = Math.max(dp[i + 1][j], piles[i][j]) //Max of (best from before, this) for max coins = 1   *j==0*
-            else
-                dp[i][j] = Math.max(dp[i + 1][j], piles[i][j] + dp[i][j - 1])  //Max of (best from before, this + rest of column) for max coins = j + 1
+
+            if (!piles[i][j] && colBack) {
+                console.log(i, j, colBack, i + 1, piles[i + 1][colBackGrab])
+                colBack--
+                colMax -= dp[i + 1][colBackGrab - 1] ? dp[i + 1][colBackGrab - 1] : 0 //Remove last coin grab if there
+                colMax += dp[i + 1][colBackGrab]; //Add best new coin grab
+                colBackGrab++
+            }
+
+            dp[i][j] = Math.max(dp[i + 1][j], colMax += (piles[i][j] ? piles[i][j] : 0)) //Max of (best from last | this)
+
+            if (!piles[i][j] && !colBackPoint) {
+                colBackPoint = j - 1;
+            }
         }
+        if (colBackPoint)
+            colBack = colBackPoint;
+        else
+            colBack = k - 1;
     }
-    return dp[0][k - 1] //Max value of coins at range 0 - i with j,k coins allowed
+    console.log(dp)
+    return dp[0][k - 1] //Max value of coins at range 0 - i with k coins allowed
 };
 
-//  7 15 => 7 
-//
 
 /*
     dp[i][j] represents the maximum value at range i to max, given j+1 coins allowed
 
 */
+
+
+console.log(maxValueOfCoins([[37, 88], [51, 64, 65, 20, 95, 30, 26], [9, 62, 20], [44]], 9))
