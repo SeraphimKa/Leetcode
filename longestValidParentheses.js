fec766e9ded9364c8ac39553b4ba82923b74ee8d
Seraphim <128935958+SeraphimKa@users.noreply.github.com>
2023-05-15 21:48:24 +0300

    Create longestValidParentheses.js

---
+++ longestValidParentheses.js
@@ -0,0 +1,67 @@
+var longestValidParentheses = function(s) {
+    if (s.length <= 1)
+        return 0
+    //setup
+    let i = 0;
+    while (true) {
+        if (!s[i])
+            return 0
+        if (s[i] == '(')
+            break
+        i++
+    }
+
+
+    const potential = (substr) => {
+        let balance = 1
+        let count = 1
+
+        for (let i=0; i < substr.length; i++) {
+            count++
+            if (substr[i] === '(')
+                balance++
+            else
+                balance--
+            
+            if (balance === 0)
+                return count
+        }
+        return 0
+    }
+
+    let max = 0
+    let load = 0
+
+
+    for (i; i < s.length; i++) {
+
+        if (s[i] == ')') {
+            max = Math.max(max, load)
+            load = 0
+            while (true) {
+                i++
+                if (!s[i])
+                    return max
+                if (s[i] == '(') {
+                    console.log(i, 'break')
+                    break
+                }
+            }
+        }
+            
+        let count = 0
+        if (s[i+1])
+            count = potential(s.substring(i+1))
+
+        if (count) {
+            load += count
+            i += count -1
+        }
+        else {
+            max = Math.max(max, load)
+            load = 0
+        }
+    }
+    max = Math.max(max, load)
+    return max
+};
